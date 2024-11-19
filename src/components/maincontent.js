ce08dd26b128b7addfc3a7f0afbc7e898bb0de7e
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2024-11-19 14:39:19 +0700

    add navbar and maincontent section

---
+++ src/components/maincontent.js
@@ -0,0 +1,22 @@
+export default function MainContent() {
+    return (
+        <div className="relative isolate px-6 pt-12 pb-12 lg:px-8">
+            <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+                <div className="relative"></div>
+            </div>
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                    
+                </div>
+                <div className="text-center">
+                    <h1 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-6xl">Welcome to my resume</h1>
+                    <h1 className="text-2xl mt-4 font-normal tracking-tight text-gray-900 sm:text-4xl">Bigboss | Thunyaluk Sasiwarinkul</h1>
+                    
+                    <p className="mt-6 text-pretty text-lg font-normal text-gray-600 sm:text-xl/8">I am a Computer Engineering student with a strong interest in full-stack development. I want to grow up in environments that allow me to solve problems, develop efficient systems, and learn new technologies. 
+                    I am currently seeking internship and cooperative education opportunities where I can further develop my skills, contribute to impactful projects, and collaborate with industry professionals.</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
